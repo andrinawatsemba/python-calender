251526e1fbd2a39fda24f7186acfd2727cb41aa3
andrinawatsemba <andrinawatsemba@gmail.com>
2025-04-01 17:38:30 +0300

    created calender.py

---
+++ calender.py
@@ -0,0 +1,8 @@
+import calendar
+
+def display_calendar(year, month):
+    """Display a calendar for given year and month"""
+    print(calendar.month(year, month))
+
+# Example usage:
+display_calendar(2023, 9)  # September 2023
